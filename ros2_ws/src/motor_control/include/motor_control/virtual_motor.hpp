b6d7a34141bb8d68dd6e94ee0ba1d3b5a6bfc68b
keisei4064 <21t4064b@shinshu-u.ac.jp>
2024-11-25 08:07:22 +0900

    sim_motor作成中,rvizに表示されない

---
+++ ros2_ws/src/motor_control/include/motor_control/virtual_motor.hpp
@@ -0,0 +1,110 @@
+#ifndef MOTOR_CONTROL_VIRTUAL_MOTOR_HPP
+#define MOTOR_CONTROL_VIRTUAL_MOTOR_HPP
+
+#include <cmath>
+#include <cstdio>
+
+#include "alps_cmn/actuator/i_motor.hpp"
+#include "alps_cmn/control/integrator.hpp"
+#include "alps_cmn/sensor/i_rotary_sensor.hpp"
+
+/**
+ * @brief 仮想的なモーター
+ */
+class VirtualMotor : public alps::cmn::actuator::IMotor<VirtualMotor>,
+                     public alps::cmn::sensor::IRotarySensor<VirtualMotor>
+{
+public:
+  /**
+   * @brief コンストラクタ
+   * 
+   * @param torque_constant   モーターのトルク定数
+   * @param moment_of_inertia 慣性モーメント
+   * @param 
+   * @param damper            ダンパー係数
+   * @param initial_velocity  初期角速度
+   * @param initial_angle     初期角度
+   */
+  VirtualMotor(
+    float torque_constant = 1.0f,
+    float moment_of_inertia = 1.0f,
+    float abs_static_friction = 0.05f,
+    float abs_kinetic_friction = 0.02f,
+    float damper = 1.0f,
+    float initial_velocity = 0.0f,
+    float initial_angle = 0.0f)
+  : torque_constant_{torque_constant},
+    moment_of_inertia_{moment_of_inertia},
+    abs_static_friction_{abs_static_friction},
+    abs_kinetic_friction_{abs_kinetic_friction},
+    damper_{damper}
+  {
+    velocity_integrator_.SetCurrentValue(initial_velocity);
+    angle_integrator_.SetCurrentValue(initial_angle);
+  }
+
+  /**
+   * @brief モーターを駆動する
+   * 
+   * @param output_ratio 駆動比
+   */
+  void Drive(float output_ratio)
+  {
+    drive_ratio_ = output_ratio;
+  }
+
+  /**
+   * @brief 簡単なモーターのモデルを用いてモーター駆動値を反映した角度を計算する
+   * 
+   * @return double 計算した角度
+   */
+  double GetAngle()
+  {
+    float pure_acceleration = drive_ratio_ * torque_constant_ / moment_of_inertia_;  // 純粋な加速度
+
+    float friction{0.0f};
+    if (std::abs(velocity_integrator_.GetCurrentValue()) < 0.01f) {
+      // 静止摩擦
+      // printf("static friction\n");
+      if (std::abs(pure_acceleration) < abs_static_friction_) {
+        pure_acceleration = 0.0f;
+        velocity_integrator_.SetCurrentValue(0.0f);
+      } else {
+        friction = std::copysign(abs_static_friction_, velocity_integrator_.GetCurrentValue());
+      }
+    } else {
+      // 動摩擦
+      // printf("kinetic friction\n");
+      friction = std::copysign(abs_kinetic_friction_, velocity_integrator_.GetCurrentValue());
+    }
+    float dumper_applied_acceleration =
+      pure_acceleration -
+      (damper_ * velocity_integrator_.GetCurrentValue());  // ダンパーを考慮した加速度
+    float axis_acceleration = dumper_applied_acceleration - friction;  // 摩擦を考慮した加速度
+    velocity_integrator_.Calculate(axis_acceleration);                 // 角速度を積分
+    angle_integrator_.Calculate(velocity_integrator_.GetCurrentValue());  // 角度を積分
+
+    return angle_integrator_.GetCurrentValue();
+  }
+
+private:
+  /// @brief 現在のモーター駆動比
+  float drive_ratio_{0.0f};
+  /// @brief モーターのトルク定数
+  float torque_constant_;
+  /// @brief 慣性モーメント
+  float moment_of_inertia_;
+  /// @brief 絶対静止摩擦
+  float abs_static_friction_;
+  /// @brief 絶対動摩擦
+  float abs_kinetic_friction_;
+
+  /// @brief ダンパー係数
+  float damper_;
+  /// @brief 角速度の積分器
+  alps::cmn::control::Integrator<std::chrono::high_resolution_clock> velocity_integrator_;
+  /// @brief 角度の積分器
+  alps::cmn::control::Integrator<std::chrono::high_resolution_clock> angle_integrator_;
+};
+
+#endif
---
+++ ros2_ws/src/motor_control/src/sim_motor.cpp
@@ -0,0 +1,133 @@
+#include "alps_cmn/control/motor_angle_controller.hpp"
+#include "alps_cmn/control/motor_angle_velocity_controller.hpp"
+#include "alps_cmn/control/motor_velocity_controller.hpp"
+#include "alps_cmn/util/angle.hpp"
+#include "alps_ros2/type/custom_type_param_conversion_rule.hpp"
+#include "alps_ros2/util/typed_param.hpp"
+#include "motor_control/control_target.hpp"
+#include "motor_control/ctrl_mode.hpp"
+#include "motor_control/key_input_thread.hpp"
+#include "motor_control/virtual_motor.hpp"
+#include "motor_control_interfaces/msg/target_angle.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+
+using namespace std::chrono_literals;
+
+class SimMotor : public rclcpp::Node
+{
+public:
+  SimMotor() : Node("sim_motor")
+  {
+    // タイマー作成
+    timer_ = this->create_wall_timer(20ms, std::bind(&SimMotor::CbTimer, this));
+
+    // パブリッシャー作成
+    pub_visualized_plant_ = this->create_publisher<visualization_msgs::msg::Marker>(
+      "sim_motor/visualized_plant", 1);
+
+    // サブスクライバー作成
+    sub_target_angle_ = this->create_subscription<motor_control_interfaces::msg::TargetAngle>(
+      "target_angle",
+      rclcpp::QoS(10),
+      [this](const motor_control_interfaces::msg::TargetAngle::SharedPtr msg) {
+        msg_target_angle_ = *msg;
+      });
+
+    msg_visualized_plant_.header.frame_id = "map";
+    msg_visualized_plant_.type = visualization_msgs::msg::Marker::CYLINDER;
+    msg_visualized_plant_.scale.x = 1.0;
+    msg_visualized_plant_.scale.y = 1.0;
+    msg_visualized_plant_.scale.z = 0.5;
+
+    // コールバックの登録
+    // param_angle_controller_.RegisterOnChangeCallback(
+    //   [this](const alps::cmn::control::PidParam & value) {
+    //     RCLCPP_INFO(
+    //       this->get_logger(),
+    //       "angle_controller_param: kp=%f, ki=%f, kd=%f, diff_lpf_time_const=%f",
+    //       value.kp,
+    //       value.ki,
+    //       value.kd,
+    //       value.diff_lpf_time_const.count());
+    //   });
+    // param_.RegisterOnChangeCallback([this](const double & value) { CbTargetChanged(value); });
+
+    RCLCPP_INFO(this->get_logger(), "Start SimMotor");
+  }
+
+private:
+  void CbTimer()
+  {
+    // 受信データ処理
+
+    // 制御
+    switch (ctrl_mode_) {
+      case CtrlMode::kNone:  // 待機
+        motor_.Drive(0.0f);
+        break;
+      case CtrlMode::kAngle:  // 角度制御
+        angle_controller.Control(angle_ctrl_target.ff_value);
+        // pub_angle_controller_calc_info.Publish(angle_controller.GetCalcInfo());
+        break;
+      case CtrlMode::kVelocity:  // 速度制御
+        velocity_controller.Control(velocity_ctrl_target.ff_value);
+        // pub_velocity_controller_calc_info.Publish(velocity_controller.GetCalcInfo());
+        break;
+      case CtrlMode::kAngleVelocity:  // 角度-速度制御
+        angle_velocity_controller.Control(velocity_controller, angle_velocity_ctrl_target.ff_value);
+        // pub_angle_velocity_controller_calc_info.Publish(angle_velocity_controller.GetCalcInfo());
+        break;
+    }
+
+    // vis
+    tf2::Quaternion q;
+    q.setRPY(0, 0, motor_.GetAngle());
+    msg_visualized_plant_.pose.orientation = tf2::toMsg(q);
+    msg_visualized_plant_.header.stamp = this->now();
+    pub_visualized_plant_->publish(msg_visualized_plant_);
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  motor_control_interfaces::msg::TargetAngle msg_target_angle_;
+  rclcpp::Subscription<motor_control_interfaces::msg::TargetAngle>::SharedPtr sub_target_angle_;
+  alps::ros2::util::TypedParamServer<alps::cmn::control::PidParam> param_angle_controller_{
+    *this, "angle_controller_param"};
+
+  // publisher
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub_visualized_plant_;
+
+  // モーター制御
+  CtrlMode ctrl_mode_{CtrlMode::kNone};
+
+  // 仮想モーター
+  VirtualMotor motor_;
+  visualization_msgs::msg::Marker msg_visualized_plant_;
+
+  // 制御器
+  using MotorAngleController = alps::cmn::control::
+    MotorAngleController<std::chrono::high_resolution_clock, VirtualMotor, VirtualMotor>;
+  using MotorVelocityController = alps::cmn::control::
+    MotorVelocityController<std::chrono::high_resolution_clock, VirtualMotor, VirtualMotor>;
+  using MotorAngleVelocityController =
+    alps::cmn::control::MotorAngleVelocityController<std::chrono::high_resolution_clock>;
+  // ※パラメータはここではセットしていない
+  MotorAngleController angle_controller{motor_, motor_, alps::cmn::control::PidParam{}};
+  MotorVelocityController velocity_controller{
+    motor_, motor_, alps::cmn::control::MotorVelocityControllerParam{}};
+  MotorAngleVelocityController angle_velocity_controller{alps::cmn::control::PidParam{}};
+
+  // 目標値
+  ControlTarget angle_ctrl_target;
+  ControlTarget velocity_ctrl_target;
+  ControlTarget angle_velocity_ctrl_target;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SimMotor>());
+  rclcpp::shutdown();
+  return 0;
+}
