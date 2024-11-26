769fa50be3592f6bbd1fe4a86b0151f91346bb3c
keisei4064 <21t4064b@shinshu-u.ac.jp>
2024-11-26 20:32:35 +0900

    rvizに表示できた

--- ros2_ws/src/motor_control/src/sim_motor.cpp
+++ ros2_ws/src/motor_control/src/sim_motor.cpp
@@ -4,6 +4,7 @@
 #include "alps_cmn/util/angle.hpp"
 #include "alps_ros2/type/custom_type_param_conversion_rule.hpp"
 #include "alps_ros2/util/typed_param.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "motor_control/control_target.hpp"
 #include "motor_control/ctrl_mode.hpp"
 #include "motor_control/key_input_thread.hpp"
@@ -24,8 +25,10 @@ public:
     timer_ = this->create_wall_timer(20ms, std::bind(&SimMotor::CbTimer, this));
 
     // パブリッシャー作成
-    pub_visualized_plant_ = this->create_publisher<visualization_msgs::msg::Marker>(
-      "sim_motor/visualized_plant", 1);
+    pub_visualized_plant_ =
+      this->create_publisher<visualization_msgs::msg::Marker>("sim_motor/visualized_plant", 1);
+    pub_visualized_plant_pose_ =
+      this->create_publisher<geometry_msgs::msg::PoseStamped>("sim_motor/visualized_plant_pose", 1);
 
     // サブスクライバー作成
     sub_target_angle_ = this->create_subscription<motor_control_interfaces::msg::TargetAngle>(
@@ -40,6 +43,12 @@ public:
     msg_visualized_plant_.scale.x = 1.0;
     msg_visualized_plant_.scale.y = 1.0;
     msg_visualized_plant_.scale.z = 0.5;
+    msg_visualized_plant_.color.r = 1.0;
+    msg_visualized_plant_.color.a = 1.0;
+    msg_visualized_plant_pose_.header.frame_id = "map";
+    msg_visualized_plant_pose_.pose.position.x = 0.0;
+    msg_visualized_plant_pose_.pose.position.y = 0.0;
+    msg_visualized_plant_pose_.pose.position.z = 0.25;
 
     // コールバックの登録
     // param_angle_controller_.RegisterOnChangeCallback(
@@ -87,6 +96,9 @@ private:
     msg_visualized_plant_.pose.orientation = tf2::toMsg(q);
     msg_visualized_plant_.header.stamp = this->now();
     pub_visualized_plant_->publish(msg_visualized_plant_);
+    msg_visualized_plant_pose_.pose.orientation = tf2::toMsg(q);
+    msg_visualized_plant_pose_.header.stamp = this->now();
+    pub_visualized_plant_pose_->publish(msg_visualized_plant_pose_);
   }
 
   rclcpp::TimerBase::SharedPtr timer_;
@@ -97,6 +109,7 @@ private:
 
   // publisher
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub_visualized_plant_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pub_visualized_plant_pose_;
 
   // モーター制御
   CtrlMode ctrl_mode_{CtrlMode::kNone};
@@ -104,6 +117,7 @@ private:
   // 仮想モーター
   VirtualMotor motor_;
   visualization_msgs::msg::Marker msg_visualized_plant_;
+  geometry_msgs::msg::PoseStamped msg_visualized_plant_pose_;
 
   // 制御器
   using MotorAngleController = alps::cmn::control::
